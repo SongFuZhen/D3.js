bebed43c7dd43ff5a66b7da4af019f5490d11d4c
SongFuZhen <evil_song@126.com>
2017-04-18 21:42:59 +0800

    web frame

---
+++ D3_Demo/D3_Demo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="D3_Demo.MvcApplication" Language="C#" %>
---
+++ D3_Demo/D3_Demo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(D3_Demo.Startup))]
+namespace D3_Demo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
